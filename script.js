16aee4832b8b5c503a8838ae39127a861824745b
Ravi kumar <76591348+ravi5051@users.noreply.github.com>
2022-10-10 22:36:14 +0530

    10-10-2022
    Simple Login page for a college project.

---
+++ script.js
@@ -0,0 +1,13 @@
+const x=document.getElementById('login');
+const y=document.getElementById('register');
+const z=document.getElementById('btn');
+function register(){
+    x.style.left="-100%"
+    y.style.left="10%"
+    z.style.left="50%"
+}
+function login(){
+    x.style.left="10%"
+    y.style.left="100%"
+    z.style.left="0";
+}
